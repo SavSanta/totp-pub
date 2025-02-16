0d02e4d31f0361a9559cc235689a441934f855ca
Ru Uba <21159693+SavSanta@users.noreply.github.com>
2025-02-16 09:21:12 -0500

    update to support multiple secrets via COLLECTIONS
    Rework the code flow. add support for multiple code presentation via a dictionary object.
    
    Added comments to clarify
    
    Seperate with a divider output for long running output disambiguation on console view

--- totp-pub.tcl
+++ totp-pub.tcl
@@ -1,7 +1,27 @@
+#!/usr/bin/env tclsh
 #!/usr/bin/wapptclsh
-# Actually can run by using just tclsh interpreter due to source calls of wapp below
 
-set SECRET "ENTER DATA HERE"
+# MSN Teams
+# set SECRET ""
+
+# MSN xtest 
+# set SECRET ""
+
+#IDME
+#set SECRET ""
+
+# Github
+# gihthub_p
+
+# MS NinjaE
+
+# LogGov
+
+set COLLECTIONS [dict create \
+LOGNGOV "" \
+IDME 	 "" \
+MSNXTEST ""	\
+]
 
 source /usr/local/lib/tcltk/wapp/wapp.tcl
 source /usr/local/lib/tcltk/mtotp.tcl
@@ -12,6 +32,18 @@ if {[catch {package require wapp}]} {
 }
 
 proc wapp-default {} {
+  # Local variable for CLI dividing
+  set divider "------- + ---------- + --------"
+
+  # Get outerscope variables to be recognized in this scope.
+  global SECRET
+  global COLLECTIONS
+
+  # Define helper fucnction that produces list of time for each epoch that gets printed to HTML and CLI.
+  proc pubtoken {when} {
+        list [t totp [expr {$when-30}]] [t totp $when] [t totp [expr {$when+30}]]
+    }
+
   wapp-content-security-policy {script-src 'self' 'unsafe-inline'}
 
   wapp-trim {
@@ -24,35 +56,50 @@ proc wapp-default {} {
   }
 
   wapp-trim {
-    <h1>Angehend Auftraege</h1>
+    <h1>Angehend Auftraege Auf 7007</h1>
+
   }
 
-  global SECRET
-  totp::totp create t [base32::decode $SECRET]
 
-  proc pubtoken {when} {
-        list [t totp [expr {$when-30}]] [t totp $when] [t totp [expr {$when+30}]]
+# Iterate the global collection for each entry and write to HTML
+dict for {site code} $COLLECTIONS {
+
+    totp::totp create t [base32::decode $code]
+
+    set epocas [pubtoken [clock seconds]]
+    puts "[string tolower $site] => $epocas"
+    lassign $epocas pass mid nex
+
+    wapp-subst {
+
+	<section name="Ziele">
+          <h3>%html($site)</h3>    <!--- Move this line into the div for better styling --->
+        <div>
+          <p>
+            <p id='pass'>%html($pass)</p>
+            <p id='mid'>%html($mid)</p>
+            <p id='nex'>%html($nex)</p>
+          </p>
+        </div>
+	</section>
     }
 
-    set val [pubtoken [clock seconds]]
-    puts $val
-    lassign $val pass mid nex
-    
+    ::t destroy
+}
+
+# CLI visual linebreak for console view
+puts $divider
+
+# HTML Footer with countdown timer
 wapp-trim {
-    <h3>Ziele:</h3>
-    
-      <p id='pass'>%html($pass)</p>
-      <p id='mid'>%html($mid)</p>
-      <p id='nex'>%html($nex)</p>
     <hr style='clear: both; '></hr>
-    
+
     <p style="color:blue" id='updateIn'>--</p>
     <script src='%url([wapp-param SCRIPT_NAME]/script.js)'></script>
     <script> setInterval(timerTick, 1000); </script>
   }
 
 
-  ::t destroy
 }
 
 # This should write to a logfile all requests. Extend it later to checksize and rotate.
@@ -62,7 +109,9 @@ proc wapp-before-reply-hook {} {
   append msg "Recieved Request at [wapp-param .header] \n
    \t\t with content:  [wapp-param CONTENT] \n
    \t\t from remote:   [wapp-param REMOTE_ADDR] \n
+   \t\t with U-Agent:   [wapp-param HTTP_USER_AGENT] \n
    \t\t [wapp-param .reply-code] code and reply sent [wapp-param .reply]\n\n"
+
 
   set bname [wapp-param SCRIPT_FILENAME]
   set logfile "/var/log/"
@@ -71,7 +120,6 @@ proc wapp-before-reply-hook {} {
   puts $out $msg
   close $out
 }
-
 
 # This is the javascript that takes refreshes page every 30 seconds
 proc wapp-page-script.js {} {
